919697e0aa995b65e552bac47582dce584985cc1
jun1 <jun1@pomtec.co.kr>
2024-05-22 16:47:54 +0900

    Player Component 생성

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
+import Player from "./components/Player.jsx";
+
 function App() {
   return (
     <main>
       <div id="game-container">
         <ol id="players">
-          <li>
-            <span className="player-name">Player 1</span>
-            <span className="player-symbol">X</span>
-          </li>
-          <li>
-            <span className="player-name">Player 2</span>
-            <span className="player-symbol">O</span>
-          </li>
+          <Player name="플레이어 1" symbol="X"></Player>
+          <Player name="플레이어 2" symbol="O"></Player>
         </ol>
         GAME BOARD
       </div>
