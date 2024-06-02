0f248be369451337237f94458823675594299016
umgu <wnsghk112@naver.com>
2024-06-02 22:45:16 +0900

    틱텍토 마무리 다듬기 및 컴포넌트 개선

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,12 @@ import Log from "./components/Log.jsx";
 import GameOver from "./components/GameOver.jsx";
 import { WINNING_COMBINATIONS } from "./winning-combinations.js";
 
-const initialGameBoard = [
+const PLAYERS = {
+  X: 'Player 1',
+  O: 'Player 2'
+}
+
+const INITIAL_GAME_BOARD = [
   [null, null, null],
   [null, null, null],
   [null, null, null],
@@ -22,18 +27,8 @@ function deriveActivePlayer(gameTurns) {
   return currentPlayer;
 }
 
-function App() {
-  const [ players, setPlayers] = useState({
-    X: '플레이어 1',
-    O: '플레이어 2'
-  })
-  const [gameTurns, setGameTurns] = useState([]);
-  // const [hasWinnder, setHasWinner] = useState(false);
-  // const [activePlayer, setActivePlayer] = useState("X");
-
-  const activePlayer = deriveActivePlayer(gameTurns);
-
-  let gameBoard = [...initialGameBoard.map(array => [...array])];
+function deriveGameBoard(gameTurns) {
+  let gameBoard = [...INITIAL_GAME_BOARD.map(array => [...array])];
 
   for (const turn of gameTurns) {
     const { square, player } = turn;
@@ -42,6 +37,10 @@ function App() {
     gameBoard[row][col] = player;
   }
 
+  return gameBoard;
+}
+
+function deriveWinner(gameBoard, players) {
   let winner = null;
 
   for (const combination of WINNING_COMBINATIONS) {
@@ -61,10 +60,19 @@ function App() {
     }
   }
 
+  return winner;
+}
+
+function App() {
+  const [players, setPlayers] = useState(PLAYERS)
+  const [gameTurns, setGameTurns] = useState([]);
+
+  const activePlayer = deriveActivePlayer(gameTurns);
+  const gameBoard = deriveGameBoard(gameTurns);
+  const winner = deriveWinner(gameBoard, players);
   const hasDraw = gameTurns.length === 9 && !winner;
 
   function handleSelectSquare(rowIndex, colIndex) {
-    // setActivePlayer((curActivePlayer) => (curActivePlayer === "X" ? "O" : "X"));
     setGameTurns((prevTurns) => {
       const currentPlayer = deriveActivePlayer(prevTurns);
 
@@ -84,7 +92,7 @@ function App() {
   function handlePlayerNameChange(symbol, newName) {
     setPlayers(prevPlayers => {
       return {
-        ...prevPlayers, 
+        ...prevPlayers,
         [symbol]: newName
       }
     })
@@ -94,13 +102,13 @@ function App() {
       <div id="game-container">
         <ol id="players" className="highlight-player">
           <Player
-            initialName="플레이어 1"
+            initialName={PLAYERS.X}
             symbol="X"
             isActive={activePlayer === "X"}
             onChangeName={handlePlayerNameChange}
           ></Player>
           <Player
-            initialName="플레이어 2"
+            initialName={PLAYERS.O}
             symbol="O"
             isActive={activePlayer === "O"}
             onChangeName={handlePlayerNameChange}
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -17,13 +17,11 @@ export default function Player({ initialName, symbol, isActive, onChangeName }) 
   }
 
   let editablePlayerName = <span className="player-name">{playerName}</span>;
-  //   let btnCaption = "Edit";
 
   if (isEdting) {
     editablePlayerName = (
       <input type="text" required value={playerName} onChange={handleChange} />
     );
-    // btnCaption = "Save";
   }
   return (
     <li className={isActive ? "active" : undefined}>
